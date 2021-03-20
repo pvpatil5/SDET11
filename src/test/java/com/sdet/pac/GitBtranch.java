7ab38430d3fd95f5d2c0de217645a164c8e857a7
pvpatil5 <pvpatil123@yahoo.com>
2021-03-20 13:27:46 +0530

    Branch Created

---
+++ src/test/java/com/sdet/pac/GitBtranch.java
@@ -0,0 +1,10 @@
+package com.sdet.pac;
+
+public class GitBtranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git branch created");
+	}
+
+}
