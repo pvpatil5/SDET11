184007f40c3015ce8dab4301d596f67f51ab9576
pvpatil5 <pvpatil123@yahoo.com>
2021-03-20 12:19:31 +0530

    SDET11 first commit

---
+++ src/test/java/com/sdet/pac/GItCMD2.java
@@ -0,0 +1,11 @@
+package com.sdet.pac;
+
+public class GItCMD2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("CLass 2 created");
+	}
+
+}
---
+++ src/test/java/com/sdet/pac/GitCMD1.java
@@ -0,0 +1,11 @@
+package com.sdet.pac;
+
+public class GitCMD1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("CLass 1 created");
+
+	}
+
+}
